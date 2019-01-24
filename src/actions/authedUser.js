351a37288f85205f3ef2b490a62a90a50f43fcc4
Nick Anthony <nicholas_anthony@brown.edu>
2019-01-24 07:33:37 -0500

    Added default export to actions and added some dependencies

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,6 +1,6 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 
-export function setAuthedUser (authedId) {
+export default function setAuthedUser (authedId) {
   return {
     type: SET_AUTHED_USER,
     authedId
--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,6 +1,6 @@
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 
-export function receiveQuestions (questions) {
+export default function receiveQuestions (questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions
