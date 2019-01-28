8a062d2d529f34e7511cc1435cf1ec1eb9b59777
Nick Anthony <nicholas_anthony@brown.edu>
2019-01-28 07:33:40 -0500

    Filter out answered and unanswered questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,24 +4,65 @@ import { connect } from 'react-redux';
 class Home extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <h3>Home</h3>
-        <ul>
-          {this.props.questionIds.map((id) =>
-            <li key={id}>
-              <div>Question Id: {id}</div>
-            </li>
-          )}
-        </ul>
-      </div>
+        <div>
+          <h3>ALL QUESTIONS</h3>
+          <ul>
+            {this.props.questionIds.map((id) =>
+              <li key={id}>
+                <div>Ans Question Id: {id}</div>
+              </li>
+            )}
+          </ul>
+          <h4>UNANSWERED QUESTIONS</h4>
+          <ul>
+            {this.props.unansweredQIds.map((id) =>
+              <li key={id}>
+                <div>Unans Question Id: {id}</div>
+              </li>
+            )}
+          </ul>
+          <h4>ANSWERED QUESTIONS</h4>
+          <ul>
+            {this.props.answeredQIds.map((id) =>
+              <li key={id}>
+                <div>Ans Question Id: {id}</div>
+              </li>
+            )}
+          </ul>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-function mapStateToProps( {questions} ){
-  return {
-    questionIds: Object.keys(questions)
+
+function mapStateToProps( { questions, authedUser } ){
+  var unansweredQIds = Object.keys(questions)
       .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+      .filter((qId) => (
+        questions[qId].optionOne.votes.indexOf(authedUser) < 0
+        &&
+        questions[qId].optionTwo.votes.indexOf(authedUser) < 0
+      ))
+  var answeredQIds = Object.keys(questions)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+      .filter((qId) => (
+        questions[qId].optionOne.votes.indexOf(authedUser) > -1
+        ||
+        questions[qId].optionTwo.votes.indexOf(authedUser) > -1
+      ))
+  console.log(questions)
+  return {
+    // answeredQIds: Object.keys(answeredQs)
+    //   .sort((a,b) => answeredQs[b].timestamp - answeredQs[a].timestamp),
+    // unansweredQIds: Object.keys(unansweredQs)
+    //   .sort((a,b) => unansweredQs[b].timestamp - unansweredQs[a].timestamp),
+    questionIds: Object.keys(questions)
+        .sort((a,b) => questions[b].timestamp - questions[a].timestamp),
+    unansweredQIds,
+    answeredQIds,
   };
 }
 export default connect(mapStateToProps)(Home);
